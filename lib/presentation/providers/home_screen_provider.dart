218fbc02ea248c0b9eff13f44e53b055fcbb9188
hyejoeng <144520192+hyejoeng@users.noreply.github.com>
2024-01-04 17:58:45 +0900

    2024-01-04 메인화면 작성

--- lib/presentation/providers/home_screen_provider.dart
+++ lib/presentation/providers/home_screen_provider.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:show_time/presentation/utils.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    body: Text('Home Screen'),
-  );
+  Widget build(BuildContext context) => SafeArea(
+        child: Scaffold(
+          body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.transparent,
+            child: Column(
+              children: [
+                Container(
+                  width: screensize(context).width,
+                  height: screensize(context).height * 0.15,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.2),
+                      borderRadius:
+                          BorderRadius.vertical(bottom: Radius.circular(15))),
+                  child: Image.asset('asset/images/worldSkills_logo.png', width: screensize(context).width * 0.7,),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
 }
---
+++ lib/presentation/utils.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Size screensize(BuildContext context) => MediaQuery.sizeOf(context);
+
+TextTheme textTheme(BuildContext context) => Theme.of(context).textTheme;
